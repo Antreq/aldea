0034159511a1dae12d3f1c063bcd6f6b716b9926
Arturo Antonio Bravo Moreno <39013275+Antreq@users.noreply.github.com>
2019-10-22 09:10:31 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,4 +1,6 @@
 <?php
+$DATABASE_URL=parse_url(‘postgres://rcccjxbzwjjktn:d8cce3c15826ab52de870fc2d4b374b097582165d34793c5b0412168fa2dee70@ec2-54-83-9-169.compute-1.amazonaws.com:5432/dcgov1cp7c82s
+’);
 
 use Illuminate\Support\Str;
 
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +67,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'], '/'),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
