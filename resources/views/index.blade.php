b26cab7e5a9c6f8ae81705fb179e3ac0349e414f
Arturo Antonio Bravo Moreno <39013275+Antreq@users.noreply.github.com>
2019-10-22 08:09:21 -0500

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,89 +10,7 @@
     </div>
 
     <!-- ##### Header Area Start ##### -->
-    <header class="header-area">
-
-        <!-- Top Header Area -->
-        <div class="top-header">
-            <div class="container h-100">
-                <div class="row h-100">
-                    <div class="col-12 h-100">
-                        <div class="header-content h-100 d-flex align-items-center justify-content-between">
-                            <div class="academy-logo">
-                                <a href="{{ url('/') }}"><img src="img/core-img/AldeaLogo.png" alt=""></a>
-                            </div>
-                            <div class="login-content">
-                                @if (Route::has('login'))
-                                    <div class="top-right links">
-                                        @auth
-                                            <a href="{{ url('/home') }}">Perfil</a>
-                                        @else
-                                            <a href="{{ route('login') }}">Iniciar seci�n  </a>
-
-                                            @if (Route::has('register'))
-                                                <a href="{{ route('register') }}"> Registrarse</a>
-                                            @endif
-                                        @endauth
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Navbar Area -->
-        <div class="academy-main-menu">
-            <div class="classy-nav-container breakpoint-off">
-                <div class="container">
-                    <!-- Menu -->
-                    <nav class="classy-navbar justify-content-between" id="academyNav">
-
-                        <!-- Navbar Toggler -->
-                        <div class="classy-navbar-toggler">
-                            <span class="navbarToggler"><span></span><span></span><span></span></span>
-                        </div>
-
-                        <!-- Menu -->
-                        <div class="classy-menu">
-
-                            <!-- close btn -->
-                            <div class="classycloseIcon">
-                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
-                            </div>
-
-                            <!-- Nav Start -->
-                            <div class="classynav">
-                                <ul>
-                                    <li><a href="index.html">Home</a></li>
-                                    <li><a href="#">Proyectos</a>
-                                        <ul class="dropdown">
-                                            <li><a href="index.html">Crear</a></li>
-                                            <li><a href="catalogo.html">Catalogo</a></li>
-                                            <li><a href="course.html">Premium</a></li>
-                                        </ul>
-                                    </li>
-                                   
-                                    <li><a href="about-us.html">Nosotros</a></li>
-                                    <li><a href="faq.html">FAQ</a></li>
-                                    <li><a href="contact.html">Contacto</a></li>
-                                </ul>
-                            </div>
-                            <!-- Nav End -->
-                        </div>
-
-                        <!-- Calling Info -->
-                        <div class="calling-info">
-                            <div class="call-center">
-                                <a href="tel:+654563325568889"><i class="icon-telephone-2"></i> <span>(+65) 456 332 5568 889</span></a>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
+    @extends('layouts.header')
     <!-- ##### Header Area End ##### -->
 
     <!-- ##### Hero Area Start ##### -->
@@ -428,7 +346,7 @@
                             </div>
                             <div class="single-contact d-flex mb-30">
                                 <i class="icon-placeholder"></i>
-                                <p>Calle Urbano Villalón 500, La Ladrillera, 78369 San Luis, S.L.P.,México</p>
+                                <p>Calle Urbano VillalÃ³n 500, La Ladrillera, 78369 San Luis, S.L.P.,MÃ©xico</p>
                             </div>
                             <div class="single-contact d-flex mb-30">
                                 <i class="icon-telephone-1"></i>
