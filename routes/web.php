583343cea3efbbd60eb298fb32b85246c275362a
Arturo Antonio Bravo Moreno <39013275+Antreq@users.noreply.github.com>
2019-10-22 07:42:42 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,38 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/catalogo', function () {
+    return view('catalogo');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/course', function () {
+    return view('course');
+});
+
+Route::get('/new', function () {
+    return view('crear');
+});
+
+Route::get('/elements', function () {
+    return view('elements');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
+
 
 
 Auth::routes();
